f9612b3769a69d150cedac3b6583324142f2f4ef
Ramanand Thakur <ramanandt35@gmail.com>
2026-02-20 23:48:01 +0530

    minimized time string length

--- utils/HelperFunctions.ts
+++ utils/HelperFunctions.ts
@@ -4,8 +4,8 @@ export const shortAddress = (address: string, charsToShow: number = 4) => {
 
 export const timeAgo = (ts: number) => {
   const sec = Math.floor(Date.now() / 1000 - ts);
-  if (sec < 60) return `${sec}s ago`;
-  if (sec < 3600) return `${Math.floor(sec / 60)}m ago`;
-  if (sec < 86400) return `${Math.floor(sec / 3600)}h ago`;
-  return `${Math.floor(sec / 86400)}d ago`;
+  if (sec < 60) return `${sec}s`;
+  if (sec < 3600) return `${Math.floor(sec / 60)}m`;
+  if (sec < 86400) return `${Math.floor(sec / 3600)}h`;
+  return `${Math.floor(sec / 86400)}d`;
 };
