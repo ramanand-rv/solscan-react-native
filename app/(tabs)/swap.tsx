ea872a359da5aaad29adc6ab1f10c3b44db5cc7c
Ramanand Thakur <ramanandt35@gmail.com>
2026-02-24 02:18:15 +0530

    swap screen added

--- app/(tabs)/swap.tsx
+++ app/(tabs)/swap.tsx
@@ -12,51 +12,101 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function swap() {
-  const [fromToken, setfromToken] = useState("");
-  const [inputValue, setinputValue] = useState("");
+  const [fromToken, setfromToken] = useState("SOL");
+  const [fromAmount, setfromAmount] = useState("0.28679");
+  const [toToken, setToToken] = useState("ETH");
+  const [toAmount, setToAmount] = useState("0.4782");
 
   // Calculate font size based on length
-  const getFontSize = () => {
-    const baseSize = 24;
+  const getFontSize = (isFrom: boolean) => {
+    const baseSize = 16;
     // const minSize = 12;
-    const length = inputValue.length;
+    const length = isFrom ? fromAmount.length : toAmount.length;
 
-    return baseSize - (length - 10);
+    return baseSize - (length * 0.6 - 10);
   };
 
   return (
     <SafeAreaView style={styles.safe}>
+      <Text style={styles.title}>Swap Tokens</Text>
       <ScrollView style={styles.scroll}>
-        <Text style={styles.title}>Swap Tokens</Text>
-
-        {/* Card */}
-        <View style={styles.fromCard}>
+        {/* From card */}
+        <View style={styles.fromToToken}>
           <View style={styles.tokenQtyContainer}>
             {/* token selection dropdown */}
-            <TouchableOpacity style={styles.fromTokenContainer}>
+            <TouchableOpacity style={styles.fromToTokenContainer}>
               <Image
                 source={{
                   uri: "https://cryptologos.cc/logos/solana-sol-logo.png?v=040",
                 }}
-                style={[styles.fromTokenIcon]}
+                style={[styles.fromToTokenIcon]}
               />
-              <Text style={styles.fromTokenName}>SOL</Text>
+              <Text style={styles.fromToTokenName}>{fromToken}</Text>
               <Ionicons name="chevron-down" color="#Ffffff" size={24} />
             </TouchableOpacity>
 
             {/* Token qty input */}
             <TextInput
-              style={[styles.fromTokenInput, { fontSize: getFontSize() }]}
+              style={[styles.fromToTokenInput, { fontSize: getFontSize(true) }]}
               keyboardType="number-pad"
-              // value={inputValue}
-              placeholder={inputValue.length == 0 ? "0.00" : ""}
-              placeholderTextColor="#5f3cb0"
-              onChangeText={(text) => setinputValue(text)}
+              value={fromAmount}
+              placeholder={"0"}
+              placeholderTextColor="#666"
+              onChangeText={setfromAmount}
+              maxLength={18}
             />
           </View>
-
-          <View></View>
+          {/* Balance text */}
+          <View style={styles.tokenBalanceContainer}>
+            <Text style={styles.balanceText}>Balance: 0.80085 {fromToken}</Text>
+            <Text style={styles.usdText}>${455.796}</Text>
+          </View>
         </View>
+
+        {/* Swap arrow  */}
+        <View style={styles.swapArrowContainer}>
+          <TouchableOpacity style={styles.swapArrow}>
+            <Ionicons name="swap-vertical" color="#FFFFFF" size={24} />
+          </TouchableOpacity>
+        </View>
+
+        {/* To card */}
+        <View style={styles.fromToToken}>
+          <View style={styles.tokenQtyContainer}>
+            {/* token selection dropdown */}
+            <TouchableOpacity style={styles.fromToTokenContainer}>
+              <Image
+                source={{
+                  uri: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=040",
+                }}
+                style={[styles.fromToTokenIcon]}
+              />
+              <Text style={styles.fromToTokenName}>{toToken}</Text>
+              <Ionicons name="chevron-down" color="#Ffffff" size={24} />
+            </TouchableOpacity>
+
+            {/* Token qty input */}
+            <TextInput
+              style={[styles.fromToTokenInput, { fontSize: getFontSize(false) }]}
+              keyboardType="number-pad"
+              value={toAmount}
+              placeholder={"0"}
+              placeholderTextColor="#666"
+              onChangeText={setToAmount}
+              maxLength={18}
+            />
+          </View>
+          {/* Balance text */}
+          <View style={styles.tokenBalanceContainer}>
+            <Text style={styles.balanceText}>Balance: 0.80085 {toToken}</Text>
+            <Text style={styles.usdText}>${455.796}</Text>
+          </View>
+        </View>
+
+        {/* Swap button */}
+        <TouchableOpacity style={styles.swapBtn}>
+          <Text style={{ fontSize: 18, fontWeight: "500" }}>Swap</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
@@ -69,20 +119,22 @@ const styles = StyleSheet.create({
   },
 
   scroll: {
-    marginBottom: 32,
-    paddingHorizontal: 5,
+    flex: 1,
+    paddingHorizontal: 10,
   },
+
   title: {
     color: "#FFFFFF",
     fontSize: 24,
     fontWeight: "700",
   },
 
-  fromCard: {
+  fromToToken: {
     backgroundColor: "#3A2A2A",
     paddingVertical: 40,
     paddingHorizontal: 5,
     borderRadius: 18,
+    height: "38%",
   },
 
   tokenQtyContainer: {
@@ -90,9 +142,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    marginTop: -28,
   },
 
-  fromTokenContainer: {
+  fromToTokenContainer: {
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
@@ -100,25 +153,71 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     backgroundColor: "#16161D",
     borderRadius: 50,
+    marginLeft: 20,
   },
 
-  fromTokenIcon: {
+  fromToTokenIcon: {
     width: 24,
     height: 24,
   },
 
-  fromTokenName: {
+  fromToTokenName: {
     color: "#FFFFFF",
     fontSize: 16,
   },
 
-  fromTokenInput: {
+  fromToTokenInput: {
     color: "#FFFFFF",
-    fontWeight: "800",
+    fontWeight: "600",
     paddingHorizontal: 10,
-    textAlign:"right",
+    textAlign: "right",
+    width: "50%",
+    marginRight: 10,
+    height: "80%",
+  },
 
-    borderWidth: 1,
-    borderColor: "white",
+  tokenBalanceContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 10,
+    paddingHorizontal: 14,
+    marginBottom: -18,
+  },
+
+  balanceText: {
+    color: "rgba(235, 235, 245, 0.6)",
+    fontSize: 14,
+    marginLeft: 10,
+  },
+
+  usdText: {
+    color: "#14F195",
+    fontSize: 14,
+    marginRight: 10,
+  },
+
+  swapArrowContainer: {
+    alignItems: "center",
+    marginVertical: -20,
+    zIndex: 1,
+  },
+
+  swapArrow: {
+    backgroundColor: "#16161D",
+    padding: 10,
+    borderRadius: 50,
+    borderWidth: 0.3,
+    borderColor: "rgba(235, 235, 245, 0.3)",
+  },
+
+  swapBtn: {
+    backgroundColor: "#14F195",
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    alignItems: "center",
+    marginTop: 20,
+    marginHorizontal: 5,
   },
 });
